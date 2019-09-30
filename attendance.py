fd0b7f956ae11c72bd9a3336cac654faeda9670f
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-09-30 16:49:39 +0530

    Added comments and some indentation.

--- attendance.py
+++ attendance.py
@@ -1,35 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sept 29 4:15:23 2019
+@author: Shashwat Kathuria
+"""
+
+# Importing required libraries
 import requests, re
 from bs4 import BeautifulSoup
 from getpass import getpass
 
+# Initializing required variables
 baseUrl = "http://intra.iitj.ac.in:8080/Aryabhatta/"
 loginUrl = "login.do"
 attendanceUrl = "attendanceReport.do"
-
 courses = ['CS311', 'CS312', 'CS313', 'CS314', 'HS311', 'HSL4020']
-temp = []
-for course in courses:
-    temp.append((course,"attendanceViewStudent.do?code=" + course + "&opt=View" ))
+temp = [(course,"attendanceViewStudent.do?code=" + course + "&opt=View" ) for course in courses]
 courses = temp
 
+# Pattern for attendance matching
 AttendancePattern = re.compile("^[0-9]+ / [0-9]+ %$")
 
 username = "kathuria.1"
-
+# Hidden input in console by getpass
 password = getpass("Enter password for " + username + " : ")
-
 payload = {
     "userid" : username,
     "password" : password
 }
 
 def main():
+
+    # Initializing a session object
     requestSession = requests.session()
+
+    # Logging in
     loginResult = requestSession.post(
         baseUrl + loginUrl,
         data = payload,
         headers = dict(referer = baseUrl + loginUrl)
     )
+
+    # Checking if successfully logged in and required page opens correctly
     attendanceResult = requestSession.get(baseUrl + attendanceUrl)
     if attendanceResult.status_code != 200:
         print("Invalid credentials. Login again.")
@@ -38,6 +49,8 @@ def main():
         print("=================")
         print("ATTENDANCE REPORT")
         print("=================")
+        # Scraping attendance page and appropriately printing attendance and
+        # course title in console
         for course in courses:
             attendanceScraper = BeautifulSoup(requestSession.get(baseUrl + course[1]).content.decode(), "html.parser")
             courseTitle = attendanceScraper.find('h3', {'class' : 'blank1'})
@@ -56,8 +69,6 @@ def main():
                 print("Lab      : " + attendances[1])
             print()
         print("=================")
-
-
 
 if __name__ == "__main__":
     main()
